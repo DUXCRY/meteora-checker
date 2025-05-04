81fa6bdaf69ffb22fd85156439cf83166c158a12
Dadan Ramadhan <88007362+DUXCRY@users.noreply.github.com>
2025-05-04 23:35:12 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@ import "./index.css";
 import App from "./App";
 import Footer from "./Footer";
 import reportWebVitals from "./reportWebVitals";
-import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <Analytics />
     <Footer />
   </React.StrictMode>
 );
